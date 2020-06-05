1e5ef44ce4e82d9f9de6bdf752157b293e2f47ac
Alex Matyushkin <Matyushkin.A.V@sberbank.ru>
2020-06-05 18:54:24 +0300

    Initial Commit

---
+++ SkyEngTranslator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkyEngTranslator
+//
+//  Created by 17503583 on 05.06.2020.
+//  Copyright © 2020 Aleksey Matyushkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
